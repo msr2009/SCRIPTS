a32c715a515f8e53a412665ba71dfd5b76680432
msr2009 <mattrich@uw.edu>
2015-12-14 01:15:26 -0800

    added heatmap generation code

--- mutCas9-oligo-score.py
+++ mutCas9-oligo-score.py
@@ -25,15 +25,23 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+from matplotlib_tools import remappedColorMap
 
 
-def main(grna, seq, blosum, mdist, fasta_name, forceC, pre_out):
+def main(grna, seq, blosum, mdist, fasta_name, forceC, pre_out, heatmap):
 	#open an output file
 	dat_out = open(pre_out+".cda1.dat", "w")
 	
 	targets = []
 	score_min = [float('Inf')]
 	score_max = [-float('Inf')]
+
+	#initialize the heatmap even if we don't use it
+	amino_acids = ['A', 'C', 'D', 'E', 'F', 
+					'G', 'H', 'I', 'K', 'L', 
+					'M', 'N', 'P', 'Q', 'R', 
+					'S', 'T', 'V', 'W', 'Y', '*']
+	hm =  { i : { x:0 for x in amino_acids } for i in range(len(seq)/3) }
 
 	if grna == None:
 		#first find all targets
@@ -50,40 +58,53 @@ def main(grna, seq, blosum, mdist, fasta_name, forceC, pre_out):
 
 	for t in targets:
 		blos_scores = [] #store all our blosum scores here
+		h_aa = []
+		h_res = []
 		strand = 1
 	
 		#score each base in each target for possible mutations
 		for p in range(len(t[2])):
 			if t[3] == "+":
 				res = (t[0]+p)/3
+				h_res.append(res)
 				pos = (t[0]+p)%3
 				aa = seq[res*3:res*3+3]
 				if forceC and t[2][p] != "C":	#require that base is cytosine
 					blos_scores.append([np.nan, np.nan, np.nan, np.nan])				
+					h_aa.append([np.nan, np.nan, np.nan, np.nan])	
 				else:
 					blos_scores.append(blosAA(aa, pos, blosum))				
-					
+					h_aa.append(blosAA(aa, pos, None))				
+				
 			if t[3] == "-":
 				res = (t[0]-1-p)/3 #<<<have to decrement these by 1
+				h_res.append(res)
 				pos = (t[0]-1-p)%3
 				aa = seq[res*3:res*3+3]
 				strand = -1
-				if forceC and t[2][p] != "G":
+				if forceC and t[2][p] != "C":
 					blos_scores.append([np.nan, np.nan, np.nan, np.nan])				
+					h_aa.append([np.nan, np.nan, np.nan, np.nan])
 				else:	
 					blos_scores.append(blosAA(aa, pos, blosum))
-				
+					h_aa.append(blosAA(aa, pos, None))
 
 		#transpose blos_scores to same orientation as mutation distributions
-		blos_scores = np.array(blos_scores).transpose()
-		
+		blos_scores = np.array(blos_scores).transpose()		
+		h_scores = np.array([[int(isinstance(m, basestring)) for m in p ] \
+								for p in h_aa]).transpose()
+	
 		#normalize blos_scores based on empirical mutation distribution
 		if mdist:
 			if t[3] == "+":
 				blos_scores = np.multiply(blos_scores, mdist[0])
+				h_scores = np.multiply(h_scores, mdist[0])
 			else:
-				blos_scores = np.multiply(blos_scores, mdist[1])
-		
+				blos_scores = np.multiply(blos_scores, mdist[1])			
+				h_scores = np.multiply(h_scores, mdist[1])
+		#add new data to heatmap dictionary	
+		hm = updateHeatmap(hm, h_scores, h_aa, h_res)
+
 		#update min and max values
 		if np.nanmin(np.nansum(blos_scores, axis=0)) < score_min:
 			score_min = np.nanmin(np.nansum(blos_scores, axis=0))
@@ -99,10 +120,12 @@ def main(grna, seq, blosum, mdist, fasta_name, forceC, pre_out):
 	plt.ylabel("Normalized BLOSUM64 score")
 	plt.title(fasta_name)
 
-	plt.savefig(pre_out+".cda1.pdf")
+	plt.savefig(pre_out+".cda1.eps", transparent=True)
 	plt.clf()
 	dat_out.close()
-			
+	
+	plotHeatmap(hm, seq, pre_out+".cda1.heatmap.eps")	
+		
 #print and plot scores
 def printAndPlot(mat, t, name, strand, outfile):
 		mat_sum = np.nansum(mat,axis=0)
@@ -116,6 +139,17 @@ def printAndPlot(mat, t, name, strand, outfile):
 			elif strand == -1:
 				plt.scatter(range(t[0], t[1], strand), mat_sum, c="b")
 	
+def updateHeatmap(heatmap, scores, aa, residue):
+	#first, transpose the scores matrix again
+	scores = scores.transpose()
+
+	for r in range(len(residue)):
+		for a in range(4):
+			try:
+				np.isnan(aa[r][a])	#I can't figure out how to work around this
+			except TypeError:
+					heatmap[residue[r]][aa[r][a]] += scores[r][a]
+	return heatmap		
 
 # lookup table for codon translation
 def lookup_codon(codon):
@@ -190,8 +224,68 @@ def blosAA(aa, pos, blos):
 	mut_aa = [ translate_sequence(mutAA(aa, pos, b)) for b in bases ]
 	for t in range(4):	
 		if aa[pos] != bases[t]:
-			bs[t] = blos[wt_aa][mut_aa[t]]
+			if blos == None:
+				bs[t] = mut_aa[t]
+			else:
+				bs[t] = blos[wt_aa][mut_aa[t]]
 	return bs					
+
+def plotHeatmap(heatmap, seq, name):
+	amino_acids = ['A', 'C', 'D', 'E', 'F', 
+					'G', 'H', 'I', 'K', 'L', 
+					'M', 'N', 'P', 'Q', 'R', 
+					'S', 'T', 'V', 'W', 'Y', '*']
+	
+	#we first need to move our heatmap to an array
+	hm = np.array([ [ heatmap[i][y] for y in amino_acids ] \
+						for i in sorted(heatmap.keys()) ])
+	
+
+	#translate wt sequence to protein, then keep track of 
+	#the wildtype at each position
+	aa_seq = translate_sequence(seq)	
+	wt_aa_index = [amino_acids.index(i) for i in aa_seq] 	
+	
+	#Columns will denote position (first 100 positions here)
+	row_labels = amino_acids
+	ticks = np.arange(0, len(aa_seq), 50)	
+	column_labels = range(len(aa_seq))
+	tlen=15
+
+	vmin=0
+	vmax=.4
+
+	fig = plt.figure()
+	fig.set_size_inches(10,25)
+	axarr = plt.subplot()
+	
+	hm_plot = axarr.imshow(hm, cmap="Greys", interpolation = 'none',
+				aspect='auto', norm=mpl.colors.Normalize(vmin, vmax),
+				extent=[0, len(amino_acids), 
+						max(column_labels)+1, min(column_labels)])
+
+	ytickloc = mpl.ticker.MultipleLocator(10)
+	ytickform = mpl.ticker.ScalarFormatter()
+	axarr.yaxis.set_major_locator(ytickloc)
+	axarr.yaxis.set_major_formatter(ytickform)
+	axarr.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator())
+	axarr.set_xticklabels(row_labels, minor=False)
+	axarr.set_xticks(np.arange(hm.shape[1])+.5, minor=False)
+	axarr.tick_params(axis='both', which='major', labelsize=26, length=tlen)
+	axarr.tick_params(axis='both', which='both', direction='in')
+	axarr.invert_yaxis()
+	
+	axarr.grid(False)
+	#Mark the WT positions
+	for i in column_labels:
+		axarr.add_patch(mpl.patches.Rectangle((wt_aa_index[i],i), 1, 1,
+						fill=False, facecolor="none", edgecolor="deepskyblue",
+						ls='dotted')) 
+	fig.colorbar(hm_plot)
+	plt.tight_layout()
+
+	plt.savefig(name)
+	plt.clf()
 
 if __name__ == "__main__":
 	
@@ -214,6 +308,9 @@ if __name__ == "__main__":
 	parser.add_argument('--out', action = 'store', type = str, 
 		dest = 'outfile', help = "prefix for output (defaults to first word \
 							of FASTA entry")
+	parser.add_argument('--heatmap', action = 'store_true', dest = 'heatmap',
+		help = 'also output heatmap of cumulative frequencies of edits for \
+				each residue', default = False)
 	args = parser.parse_args()
 
 	outfile = ""
@@ -228,10 +325,10 @@ if __name__ == "__main__":
 			if args.outfile != None:
 				main(args.grna, s[1], parseBLOSUM(args.blosum), 
 					readMutDist(args.md), fasta_name, 
-					args.forceC, args.outfile)	
+					args.forceC, args.outfile, args.heatmap)	
 			else:
 				main(args.grna, s[1], parseBLOSUM(args.blosum), 
 					readMutDist(args.md), fasta_name, 
-					args.forceC, fasta_name)	
+					args.forceC, fasta_name, args.heatmap)	
 			
 
