8de556fc79d9764a1b4db510811ac638de4a65e0
msr2009 <mattrich@uw.edu>
2017-02-06 11:53:57 -0800

    adding fasta_iter to sam_to_fullFQ.py

--- sam_to_fullFQ.py
+++ sam_to_fullFQ.py
@@ -8,7 +8,8 @@ with relevant mutations from each read (or read pair)
 Matt Rich, 02/2017
 """
 
-import pysam, fasta_iter
+import pysam
+from itertools import groupby
 
 
 def main(sam, wildtype, paired):
@@ -75,6 +76,22 @@ def printFASTQ(name, seq, qual):
 		print "+"
 		print qual
 
+
+def fasta_iter(fasta_name, noiter=False):
+		"""
+		given a fasta file. yield tuples of header, sequence
+		this by brentp from https://www.biostars.org/p/710/
+		"""
+		fh = open(fasta_name)
+		# ditch the boolean (x[0]) and just keep the header or sequence since
+		# we know they alternate.
+		faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+		for header in faiter:
+				# drop the ">"
+				header = header.next()[1:].strip()
+				# join all sequence lines to one.
+				seq = "".join(s.strip() for s in faiter.next())
+				yield header, seq
 
 if __name__ == "__main__":
 	
